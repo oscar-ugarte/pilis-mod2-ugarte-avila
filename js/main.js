66595d6e9630bec937d94917ca672eb948df203c
gon2410x <gonzalo.virtual.2020@gmail.com>
2022-08-26 21:04:40 -0300

    adding promise, fount and page icon

--- js/main.js
+++ js/main.js
@@ -1,22 +1,47 @@
-import {onClick} from "./form-event-click.js";
+import { onClick } from "./form-event-click.js";
 
 let boton = document.getElementById("enviar");
 boton.addEventListener("click", onClick);
 
 
+// evento se desencadena cuando se hace click en al ancla registrar del menu de navegacion
+let anchor = document.getElementById("anchor-registro")
+anchor.addEventListener("click", seccionRegistro)
 
 
 
-// efecto que mueve las letras a la derechas del registro-title
-let posLeter = document.querySelector(".registro-title")
-posLeter.style.left = "-400px"
-let posX = -400
+const cartelOne = ".registro-title .h2-sumate-al-evento"
+const cartelTwo = ".registro-title .h2-luego-te-contact"
 
-let b = setInterval(function(){
-    posLeter.style.left = ""+posX+"px"
-    posX = posX + 10
-    if(posX === 0){
-        clearInterval(b);
-    }
-},10);
-    
+// efecto que mueve las letras a la derechas
+const cartel = (n) => {
+    return new Promise((resolve) => {
+
+        const posLetter = document.querySelector(n)
+        posLetter.style.left = "-600px"
+        let posX = -600
+        let b = setInterval(function () {
+            posLetter.style.left = "" + posX + "px"
+            posX = posX + 10
+            if (posX === 0) {
+                clearInterval(b);
+                resolve()
+            }
+        }, 10)
+
+    })
+}
+
+function seccionRegistro() {
+
+    document.querySelector(cartelOne).style.left = "-600px"
+    document.querySelector(cartelTwo).style.left = "-600px"
+
+    cartel(cartelOne).then(() => cartel(cartelTwo))
+}
+
+
+document.querySelector(cartelOne).style.left = "-600px"
+document.querySelector(cartelTwo).style.left = "-600px"
+
+cartel(cartelOne).then(() => cartel(cartelTwo))
